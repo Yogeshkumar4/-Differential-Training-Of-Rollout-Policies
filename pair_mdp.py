87e0895515a77dd2bf139ede4f992664c67785a8
Yogesh Kumar <yogeshiitbcse@gmail.com>
2017-11-18 02:38:00 +0530

    Added PI

--- pair_mdp.py
+++ pair_mdp.py
@@ -10,7 +10,7 @@ from utils import str2bool, ACTIONS
 class pairEnv:
 
     def __init__(self, env):
-        self.env1 = env #gym.make('FrozenLake-v0')
+        self.env1 = copy.deepcopy(env)
         self.env2 = copy.deepcopy(env)
         self.states = self.env1.getnumStates()
 
@@ -25,9 +25,9 @@ class pairEnv:
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description="Implements the Environment.")
-    parser.add_argument('-side', '--side', dest='side', type=int, default=8, help='Side length of the square grid')
+    parser.add_argument('-side', '--side', dest='side', type=int, default=4, help='Side length of the square grid')
     parser.add_argument('-i', '--instance', dest='instance', type=int, default=0, help='Instance number of the gridworld.')
-    parser.add_argument('-slip', '--slip', dest='slip', type=float, default=0.4, help='How likely is it for the agent to slip')
+    parser.add_argument('-slip', '--slip', dest='slip', type=float, default=0.02, help='How likely is it for the agent to slip')
     parser.add_argument('-ml', '--maxlength', dest='maxLength', type=int, default=1000, help='Maximum number of timesteps in an episode')
     parser.add_argument('-rs', '--randomseed', dest='randomseed', type=int, default=0, help='Seed for RNG.')
     parser.add_argument('-nobf', '--noobfuscate', dest='obfuscate', type=str2bool, nargs='?', const=False, default=False, help='Whether to obfuscate the states or not')
@@ -47,5 +47,21 @@ if __name__ == '__main__':
             action = (pi[s[0]], pi[s[1]])
             s1, R, done = env.step(action)
             V[s[0]][s[1]] += alpha * (R + gamma * V[s1[0]][s1[1]] - V[s[0]][s[1]])
+
+    averageOver = 100
+    tempPi = np.empty(env0.getnumStates(), dtype=int)
+    for i in xrange(env0.getnumStates()):
+        currentMax = -10000000
+        for k in xrange(4): 
+            actionMean = 0
+            for j in xrange(averageOver):
+                ns1, r1 = env0.sampleAction(i, pi[i])
+                ns2, r2 = env0.sampleAction(i, k)
+                actionMean += (r1 - r2) + gamma*V[ns1][ns2]
+            if actionMean > currentMax:
+                currentMax = actionMean
+                tempPi[i] = k  
+    print(pi)
+    print(tempPi)                 
 
     print(V)
